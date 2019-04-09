bd790e11561f4bb074f219e22760b4879e552767
onnion <marcio.hsdl@gmail.com>
2019-04-09 09:57:42 -0300

    [master] | remove services unused

--- src/app/helpers/interceptor/interceptor.helper.ts
+++ src/app/helpers/interceptor/interceptor.helper.ts
@@ -1,48 +1,46 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor, HttpResponse, HttpErrorResponse
-} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {tap, retry} from 'rxjs/operators';
-import {AuthService} from '../../services/auth/auth.service';
-import {HandlerErrorHelpers} from '../handler-error/handler-error.helper';
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap, retry } from "rxjs/operators";
+import { AuthService } from "../../services/auth/auth.service";
+import { HandlerErrorHelpers } from "../handler-error/handler-error.helper";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root"
 })
 export class TokenInterceptor implements HttpInterceptor {
-    protected handlerErrorHelper;
+  constructor(public auth: AuthService, private handler: HandlerErrorHelpers) {}
 
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    request = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.auth.getToken()}`
+      }
+    });
 
-    constructor(public auth: AuthService, private handler: HandlerErrorHelpers) {
-        this.handlerErrorHelper = handler;
-    }
-
-
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        request = request.clone({
-            setHeaders: {
-                Authorization: `Bearer ${this.auth.getToken()}`
+    // retry(1)
+    return next.handle(request).pipe(
+      tap(
+        (event: HttpEvent<any>) => {
+          if (event instanceof HttpResponse) {
+            if (event.body.error) {
+              this.handler.handle(event);
             }
-        });
-
-        // retry(1)
-        return next.handle(request).pipe(tap((event: HttpEvent<any>) => {
-                if (event instanceof HttpResponse) {
-                    if (event.body.error) {
-                        this.handlerErrorHelper.handle(event);
-                    }
-                }
-            },
-            (error: any) => {
-                if (error instanceof HttpErrorResponse) {
-                    this.handlerErrorHelper.handle(error);
-
-                }
-            })
-        );
-    }
+          }
+        },
+        (error: any) => {
+          if (error instanceof HttpErrorResponse) {
+            this.handler.handle(error);
+          }
+        }
+      )
+    );
+  }
 }
